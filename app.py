533b577665880a38402ccaa0b4c46ab4cfeb8baa
Shivani Kalghatgi <shivanikalghatgi@gmail.com>
2025-09-11 00:19:43 +0530

    app.py

--- app.py
+++ app.py
@@ -11,6 +11,24 @@ st.set_page_config(
   layout="centered"
 )
 
+# Background Image
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as f:
+        encoded_string = base64.b64encode(f.read())
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/jpg;base64,{encoded_string.decode()});
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 add_bg_from_local("titanic_bg.jpg")
 
 # Load Model and Scaler
