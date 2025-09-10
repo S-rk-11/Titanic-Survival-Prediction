b0ee24e8ea6e8204b0ebe29b8288e72554011530
Shivani Kalghatgi <shivanikalghatgi@gmail.com>
2025-09-10 22:34:38 +0530

    app.py

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ with open ("models/logistic_model.pkl", "rb") as f:
 
 with open ("models/scaler.pkl", "rb") as f:
   scaler = pickle.load(f)
-
+  
+#App TItle
 st.title("Titanic Survival Prediction")
+st.markdown(
+  "This app predicts whether a passenger would have survived the Titanic disaster."
+  "based on input features."
+)
 
+# User Input
 Pclass = st.selectbox("Passenger Class (Pclass)", [1, 2, 3])
 Sex = st.selectbox("Sex", ["male", "female"])
 Age = st.number_input("Age", min_value=0, max_value=100, value=25)
@@ -20,21 +26,28 @@ Parch = st.number_input("Number of Parents/Children Aboard (Parch)", min_value=0
 Fare = st.number_input("Fare", min_value=0.0, value=32.2)
 Embarked = st.selectbox("Port of Embarkation", ["S", "C", "Q"])
 
-Sex = 1 if Sex == 'male' else 0
-Embarked_map = {'S': 1, 'C': 0, 'Q': 2}
-Embarked = Embarked_map[Embarked]
+# Data Preprocessing
+sex_encoded = 1 if Sex == 'male' else 0
+embarked_map = {"S": 1, "C": 0, "Q": 2}
+embarked_encoded = embarked_map[Embarked]
 
-input_data = pd.DataFrame([[Pclass, Sex, Age, SibSp, Parch, Fare, Embarked]],
-                          columns=['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked'])
+# Create input dataframe
+input_data = pd.DataFrame(
+  [[Pclass, sex_encoded, Age, SibSp, Parch, Fare, embarked_encoded]],
+  columns=['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+)
 
+# Scale input
 scaled_input = scaler.transform(input_data)
 
-prediction = model.predict(scaled_input)
-prediction_proba = model.predict_proba(scaled_input)
+# Prediction
+prediction = model.predict(scaled_input)[0]
+prediction_proba = model.predict_proba(scaled_input)[0][1]
 
-if prediction[0] == 1:
-    st.success("🎉 The passenger would have survived!")
+# Output
+if prediction == 1:
+    st.success("The passenger **survived**")
 else:
-    st.error("💀 The passenger would not have survived.")
+    st.error("The passenger did **not survived**.")
 
-st.write("Survival Probability:", round(prediction_proba[0][1]*100, 2), "%")
+st.write(f"**Survival Probability:** {round(prediction_proba *100, 2)}%")
